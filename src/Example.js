eb98e6a414af8632e6925338aff09e3114627d0b
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-10 15:29:03 -0800

    Added synonym and example components and matching html

---
+++ src/Example.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Example(props){
+    if (props.example){
+        return(
+            <p><strong>Example: </strong> 
+            {props.example}</p>
+        )
+    } else{
+        return null;
+    }
+}
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import Example from "./Example";
+
 
 export default function Meaning(props){
+    console.log(props)
     return(
         <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function(definition, index)
-        { 
-            return (
-            <div key={index}>
-                <p>
-                    {definition.definition}
-                    <br />
-                    <em>{definition.example}</em>
-                </p>
-                </div>
-        );
-        })}
+        <p><strong>Definition: </strong> {props.meaning.definitions[0].definition}</p>
+        
+        <Example example={props.meaning.definitions[0].example} />
+        <Synonyms synonyms={props.meaning.synonyms}/>
         </div>
-    );
+                    
+        );
 }
