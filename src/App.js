a0dff9c0d054965598f6f3b4b1def2b4194d50eb
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-09 23:41:24 -0800

    Added dictionary component and css file with an active search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-     
-     Hello
+     <div className="container">
+      <header className="App-header text-center">
+        Dictionary
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">This project was coded by Toni Dodd and is open sourced on Github</footer>
+     </div>
     </div>
   );
 }
