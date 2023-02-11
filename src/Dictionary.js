9be6dce9cf8cfab5e388e974ba19c9f2575fa427
ToniDodd <121724318+ToniDodd@users.noreply.github.com>
2023-02-10 19:53:29 -0800

    added pexels api, photos component and css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css"
 
 
@@ -8,14 +9,25 @@ export default function Dictionary(){
     let [keyword, setKeyword] = useState("coding");
     let [results, setResults] = useState("");
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
 function handleResponse(response) {
     setResults(response.data[0]);
 }
+
+function getPhotos(response){
+    setPhotos(response.data.photos);
+
+}
+
 function search(){
  //documentation: https://dictionaryapi.dev/
  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
  axios.get(apiUrl).then(handleResponse);
+
+ let pexelsApiKey = "1spsDNaFsK52l1ZtgfjT6HXqpbXW2jC5V2UhOtiRt0JmOBRGkgW5K9Xu"
+ let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`
+ axios.get(pexelsApiUrl,{ headers: {"Authorization" : `Bearer${pexelsApiKey}`}}).then(getPhotos);
 }
 
 function handleSubmit(event){
@@ -38,7 +50,8 @@ return(
         <p>What word do you want to look up?</p>
         <form onSubmit={handleSubmit}> 
             <input type="search" placeholder="Coding" onChange={keywordChange} />           
-            <Results results={results}/>          
+            <Results results={results}/> 
+             <Photos photos={photos} />        
         </form>
          </section>
     </div>);
